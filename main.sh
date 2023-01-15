0caeafc4d58cb0abed5c8df9f89e1ef88ecdede4
redball1017 <bingyuanfn@gmail.com>
2023-01-15 16:55:16 +0800

    更新：支持自动获取下载得到的Core文件名

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
+echo Setting variables
 UUID=$(curl -s $REPLIT_DB_URL/UUID)
 V2RAY_DOWNLOAD_URI=$(curl -s "https://api.github.com/repos/XTLS/Xray-core/releases/latest"  | jq .assets[14].browser_download_url  | sed s/\"//  | sed s/\"//)
+filename_list=(${V2RAY_DOWNLOAD_URI//\// })
+V2RAY_FILE_NAME=${filename_list[7]}
+echo $NAME
 echo Downloading V2ray
 if [ ! -f "$V2RAY_FILE_NAME" ]; then
   wget "$V2RAY_DOWNLOAD_URI" &> /dev/null
